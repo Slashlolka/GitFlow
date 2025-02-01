1e09bc2378ff70db68b005a710356598b28ad60e
Slashlolka <123186765+Slashlolka@users.noreply.github.com>
2025-02-02 01:24:59 +0600

    Update GitScript.cs

--- GitScript.cs
+++ GitScript.cs
@@ -1,1 +1,1 @@
-[{"Name":"Stepan","DMG":2.0,"MinHP":1.0,"MaxHP":2.0},{"Name":"Sasha","DMG":20.0,"MinHP":150.0,"MaxHP":200.0}]
+[{"Name":"NewStepan","DMG":10000000.0,"MinHP":1.0,"MaxHP":2.0},{"Name":"NewSasha","DMG":20.0,"MinHP":150.0,"MaxHP":200.0}]
