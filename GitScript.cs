8e2b96483e7cda6db0c86748aa7f8ea3c50142f5
Slashlolka <123186765+Slashlolka@users.noreply.github.com>
2025-02-02 00:05:26 +0600

    Update GitScript.cs

--- GitScript.cs
+++ GitScript.cs
@@ -1,5 +1,5 @@
-<One/>10<One\> 
-<Two/>10<Two\> 
-<Three/>10<Three\> 
-<Four/>10<Four\> 
-<Five/>10<Five\> 
+</One>10<One\> 
+</Two>10<Two\> 
+</Three>10<Three\> 
+</Four>10<Four\> 
+</Five>10<Five\> 
